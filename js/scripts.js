f3f9a18f35baec17420d66737175c6fd99160458
DJ <83621102+DcurleyII@users.noreply.github.com>
2021-12-27 20:12:41 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert('Hello world');
-let favoriteFood = 'FatBurger';
-document.write('FatBurger');
+//pokemonList stays blank//
+let pokemonList = [];
+let pokemonList = {
+    name: 'Bulbasaur'
+    type:'grass','poison'
+    height: 7
+  };
+let pokemonList = {
+    name: 'Bayleef'
+    type: 'grass','poison'  
+    height: 7
+};
+let pokemonList = {
+    name: 'Charizard'
+    type: 'fire'
+    height: '7'
+};

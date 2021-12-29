8bf91de300227bf4f637ef3e85bd5e8ee04294ff
DJ <83621102+DcurleyII@users.noreply.github.com>
2021-12-28 21:41:59 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,15 @@
-//pokemonList stays blank//
-let pokemonList = [];
-let pokemonList = {
-    name: 'Bulbasaur'
-    type:'grass','poison'
-    height: 7
-  };
-let pokemonList = {
-    name: 'Bayleef'
-    type: 'grass','poison'  
-    height: 7
-};
-let pokemonList = {
-    name: 'Charizard'
-    type: 'fire'
-    height: '7'
-};
+let pokemonList = [
+    {name: "Bulbasaur", type: "grass, poison", height: 0.7},
+    {name: "Omastar", type: "water, rock", height: 1},
+    {name: "Charizard", type: "fire, flying", height: 1.7}
+];
+
+for (let i=0; i < pokemonList.height; i++){
+if (pokemonList[i].height < 1 && pokemonList[i].height> 0.6){
+    document.write (pokemonList[i].name + " small fry");
+} else if (pokemonList[i].height < 0.6 && pokemonList[i].height> 1){
+    document.write (pokemonList[i].name + " Whoa Big Fella");
+} else {
+    document.write (pokemonList[i].name + " Averag Joe");
+ } 
+}   

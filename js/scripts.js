e994fc308153276295ee4b7859b0aafc49fb1137
DJ <83621102+DcurleyII@users.noreply.github.com>
2021-12-29 20:04:44 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,10 @@ let pokemonList = [
     {name: "Charizard", type: "fire, flying", height: 1.7}
 ];
 
-for (let i=0; i < pokemonList.height; i++){
-if (pokemonList[i].height < 1 && pokemonList[i].height > 0.6){
+for (let i = 0; i < 4; i++){
+if (pokemonList[i].legnth < 1 && pokemonList[i].legnth > 0.6){
     document.write (pokemonList[i].name + " small fry");
-} else if (pokemonList[i].height < 2 && pokemonList[i].height > 1){
+} else if (pokemonList[i].legnth < 2 && pokemonList[i].legnth > 1){
     document.write (pokemonList[i].name + " Whoa Big Fella");
 } else {
     document.write (pokemonList[i].name + " Average Joe");
